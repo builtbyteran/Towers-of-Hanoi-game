55d63b0b84c8ba1d52c7648b51e7361e527bfa28
Teran <186217552+builtbyteran@users.noreply.github.com>
2026-01-26 19:33:28 -0700

    Installed wesbos config, switched to arrow functions

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
 // Represents 3 pegs
 let board = [[5, 4, 3, 2, 1], [], []];
 
-const printBoard = function () {
+const printBoard = () => {
   // Creates the pegs
-  const currentBoard = board.map(function (peg) {
-    return '---' + peg.join(' ');
-  });
+  const currentBoard = board.map((peg) => `---${peg.join(' ')}`);
 
   // Logs the pegs
-  currentBoard.forEach(function (peg) {
+  currentBoard.forEach((peg) => {
     console.log('peg', peg);
   });
 };
 
-const moveDisc = function (source, target) {
+const moveDisc = (source, target) => {
   // Turns the user inout to 0 based indexing for user convenience
   const sourcePeg = board[source - 1];
   const targetPeg = board[target - 1];
@@ -34,20 +32,20 @@ const moveDisc = function (source, target) {
   printBoard();
 };
 
-const checkWinner = function () {
+const checkWinner = () => {
   if (board[1].length === 5) {
     console.log(
-      "Congrats, you have successfully moved all the discs onto peg #2, you're a genius!"
+      "Congrats, you have successfully moved all the discs onto peg #2, you're a genius!",
     );
   }
   if (board[2].length === 5) {
     console.log(
-      "Congrats, you have successfully moved all the discs onto peg #3, you're a genius!"
+      "Congrats, you have successfully moved all the discs onto peg #3, you're a genius!",
     );
   }
 };
 
-document.querySelector('.reset').addEventListener('click', function () {
+document.querySelector('.reset').addEventListener('click', () => {
   // Arrays are truthy and [] === [] is false, so I came up with this
   if (board[0].length === 5 && board[1].length === 0 && board[2].length === 0) {
     console.log('The board is already set to be played on!');
