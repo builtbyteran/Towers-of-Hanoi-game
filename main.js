043c9a401cb778db90aca1cdb2093cc330d3bcf3
Teran <186217552+builtbyteran@users.noreply.github.com>
2026-01-26 18:05:11 -0700

    Refactored moveDisc, added comments

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
-let board = [[5, 4, 3, 2, 1], [], []]; // Represents the 3 pegs
+// Represents 3 pegs
+let board = [[5, 4, 3, 2, 1], [], []];
 
 const printBoard = function () {
+  // Creates the pegs
   const currentBoard = board.map(function (peg) {
     return '---' + peg.join(' ');
   });
 
+  // Logs the pegs
   currentBoard.forEach(function (peg) {
     console.log('peg', peg);
   });
+};
+
+const moveDisc = function (source, target) {
+  // Turns the user inout to 0 based indexing for user convenience
+  const sourcePeg = board[source - 1];
+  const targetPeg = board[target - 1];
+
+  // Logic for moving discs, ex: peg[disc]
+  const successfulMove =
+    (targetPeg.length === 0 && sourcePeg.length > 0) ||
+    targetPeg[targetPeg.length - 1] > sourcePeg[sourcePeg.length - 1];
+
+  // Check for invalid move before executing
+  if (!successfulMove) {
+    console.log('You cannot do that move');
+  } else {
+    targetPeg.push(sourcePeg.pop());
+  }
+
+  checkWinner();
+  printBoard();
 };
 
 const checkWinner = function () {
@@ -15,41 +39,16 @@ const checkWinner = function () {
     console.log(
       "Congrats, you have successfully moved all the discs onto peg #2, you're a genius!"
     );
-  } else if (board[2].length === 5) {
+  }
+  if (board[2].length === 5) {
     console.log(
       "Congrats, you have successfully moved all the discs onto peg #3, you're a genius!"
     );
   }
-
-  return;
-};
-
-const moveDisc = function (sourcePeg, targetPeg) {
-  let successfulMove = 'That move was successful, board is now:';
-  let error =
-    'You cannot move a larger disc on top of a smaller one, board is still:';
-
-  let originPeg = board[sourcePeg - 1];
-  let moveToPeg = board[targetPeg - 1];
-
-  if (moveToPeg.length === 0) {
-    moveToPeg.push(originPeg.pop());
-    console.log(successfulMove);
-  } else if (
-    moveToPeg[moveToPeg.length - 1] > originPeg[originPeg.length - 1]
-  ) {
-    moveToPeg.push(originPeg.pop());
-    console.log(successfulMove);
-  } else {
-    console.log(error);
-  }
-
-  checkWinner();
-  printBoard();
 };
 
 document.querySelector('.reset').addEventListener('click', function () {
-  // Arrays are truthy and [] === [] is false, so I had to write out some winded logic
+  // Arrays are truthy and [] === [] is false, so I came up with this
   if (board[0].length === 5 && board[1].length === 0 && board[2].length === 0) {
     console.log('The board is already set to be played on!');
   } else {
